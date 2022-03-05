f262416a9766e339167a2973a66bdd87aabc12ce
leenahmad <leenkaraja24@gmail.com>
2022-03-05 22:08:20 +0200

    finish challenge 5

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,109 @@
 'use strict';
 
-// Require our linked list implementation
+
+const { describe, it } = require('eslint/lib/rule-tester/rule-tester');
+
+
 const LinkedLilst = require('../index');
 
-describe('Linked List', () => {
+describe('test Linked List', () => {
+
   it('works', () => {
+
     expect(true).toBeTruthy();
+
   });
+
+
+
+  it('an empty linked list',() => {
+
+    const ll = new LinkedLilst();
+
+    expect(ll).toBeDefined();
+
+    expect(ll.head).toBeNull();
+
+  });
+
+});
+
+
+describe('first node in the linked list', () => {
+
+  it('insert in linked list', () => {
+
+    const ll = new LinkedLilst();
+
+    ll.insert(1);
+
+    expect(ll.head.value).toEqual(1);
+
+    expect(ll.head.next).toBeNull();
+
+  });
+
+
+  it('insert in linked list', () => {
+
+    const ll = new LinkedLilst();
+
+    ll.insert(1);
+
+    ll.insert(2);
+
+    expect(ll.head.value).toEqual(2);
+
+    expect(ll.head.next.value).toBe(1);
+
+  });
+});
+
+
+describe('return true or false', () => {
+
+  it('return true if value within the linked list that exists',() => {
+
+    let ll = new LinkedLilst();
+
+    ll.insert(1);
+
+    ll.insert(2);
+
+    ll.insert(3);
+
+    ll.insert(4);
+
+    expect(ll.includes(4)).toBe(true);
+
+  });
+
+
+  it('return false if linked list  does not exist',() => {
+
+    let ll = new LinkedLilst();
+
+    ll.insert(1);
+
+    ll.insert(2);
+
+    ll.insert(3);
+
+    ll.insert(4);
+
+    expect(ll.includes(5)).toBe(false);
+  });
+
+  it('return a all values in the linked list',() => {
+    let ll = new LinkedLilst();
+
+    ll.insert('a');
+
+    ll.insert('b');
+
+    ll.insert('c');
+
+    expect(ll.toString()).toBe('{ c }->{ b }->{ a }->NULL');
+  });
+
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,72 @@
 'use strict';
 
+const Node = require('./node.js');
+
 class LinkedList {
 
+  constructor() {
+    this.head = null;
+
+  }
+  //insert
+  insert(value) {
+
+    const node = new Node(value);
+
+    if (!this.head) {
+
+      this.head = node;
+    }
+
+    else {
+
+      node.next = this.head;
+
+      this.head = node;
+    }
+  }
+
+  //includes
+  includes(value) {
+
+    const node = this.head;
+
+    while (node !== null) {
+
+      if (node.value === value) {
+
+        return true;
+      }
+
+      else if (node.value !== value) {
+
+        return false;
+      }
+
+    }
+
+  }
+
+  //toString
+  toString() {
+
+    let string = '';
+
+    while (this.head !== null) {
+
+      string = string + `{ ${this.head.value} }->`;
+
+      this.head = this.head.next;
+
+    }
+
+    string = string + 'NULL';
+
+    return string;
+  }
+
 }
+
+
 
 module.exports = LinkedList;

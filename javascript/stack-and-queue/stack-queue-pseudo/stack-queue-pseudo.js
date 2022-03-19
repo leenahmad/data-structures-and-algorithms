46b08d23ba4b3618e1f300e2e444ffb8ff6e56b4
leenahmad <leenkaraja24@gmail.com>
2022-03-19 14:39:23 +0200

    finish challenge11

---
+++ javascript/stack-and-queue/stack-queue-pseudo/stack-queue-pseudo.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Stack = require('../stack');
+
+class QueueP {
+  constructor(){
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  //enqueue
+  //Arguments: value
+  //Inserts value into the PseudoQueue, using a first-in, first-out approach.
+
+  enqueue(value){
+    this.stack1.push(value);
+    return this.stack1.top;
+
+  }
+
+  dequeue(){
+    while(this.stack1.top){
+      this.stack2.push(this.stack1.pop());
+    }
+    return this.stack1.pop();
+  }
+
+
+}
+
+module.exports = QueueP;
